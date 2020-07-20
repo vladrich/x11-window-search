e97a2b00950d1119f53262736ecf15708f2876b2
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2020-07-20 11:33:33 +0300

    move a mouse pointer to the (width/2,0) pos

--- focus.sh
+++ focus.sh
@@ -4,4 +4,19 @@ id=`echo "$1" | awk -F'|' '{print $NF} END { exit $NF == "" ? 1 : 0}'` || {
     echo "usage: `basename "$0"` 'name | class | id'"
     exit 1
 }
-xdotool windowactivate "$id" mousemove --window "$id" 0 0
+
+# tries to put a mouse pointer as shown on the picture below (▲ is the
+# pointer):
+#
+# +-------------+
+# |    Title    |
+# +-------------+
+# |      ▲      |
+# |             |
+# |             |
+# +-------------+
+
+eval "`xdotool getwindowgeometry --shell "$id"`"
+x=$((WIDTH/2))
+
+xdotool windowactivate "$id" mousemove --window "$id" "$x" 0
